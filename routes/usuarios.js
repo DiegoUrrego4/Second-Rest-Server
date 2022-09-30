6740a0ec224e99512719d410a4adb341b959523e
Diego Urrego <diegourrg@gmail.com>
2022-09-30 11:54:28 -0500

    Se encripta la contraseña enviada por el usuario y se verifica que el campo del correo sea enviado de forma correcta.

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const {
   usuariosGet,
   usuariosPut,
@@ -13,7 +14,16 @@ router.get('/', usuariosGet);
 
 router.put('/:id', usuariosPut);
 
-router.post('/', usuariosPost);
+/* *
+ * El middleware es una función que se usa antes de ejecutar cualquier función
+ *  la forma correcta de usar es enviando un segundo argumento, por ejemplo:
+ !  router.post('/', middlewareCualquiera, usuariosPost);
+ ? Nota: si son varios arreglos se envían como un arreglo
+ !  Estructura:  router.post(ruta, middlewaresCualquiera, controlador);
+ */
+router.post('/', [
+  check('correo', 'El correo no es válido').isEmail(),
+], usuariosPost);
 
 router.patch('/', usuariosPatch);
 
