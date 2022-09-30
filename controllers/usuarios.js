9067c76d8b734cf05b023f6b0742e8b65641dbf4
Diego Urrego <diegourrg@gmail.com>
2022-09-30 11:24:12 -0500

    Obtención y guardado de usuarios en BD exitoso. Aún sin precauciones.

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,5 @@
 const { response, request } = require('express');
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
   const { q, nombre = 'No Name', apikey } = req.query;
@@ -18,13 +19,15 @@ const usuariosPut = (req, res = response) => {
   });
 };
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async(req, res = response) => {
   // Pequeña validación del body
-  const { nombre, edad } = req.body;
+  const body = req.body;
+  const usuario = new Usuario(body);
+
+  await usuario.save();
+
   res.json({
-    msg: 'post API - Controlador',
-    nombre,
-    edad,
+    usuario,
   });
 };
 
