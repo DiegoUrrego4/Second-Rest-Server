c5c2aa968a2d43bce6458ace9fd7c124057748de
Diego Urrego <diegourrg@gmail.com>
2022-10-03 01:23:26 -0500

    Se crean validaciones y query para obtener usuarios activossolamente.

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -5,10 +5,21 @@ const Usuario = require('../models/usuario');
 const usuariosGet = async (req = request, res = response) => {
   // const { q, nombre = 'No Name', apikey } = req.query;
   const { limite = 5, desde = 0 } = req.query;
-  const usuarios = await Usuario.find()
-    .skip(Number(desde))
-    .limit(Number(limite));
+  const query = { estado: true };
+
+  // const usuarios = await Usuario.find(query)
+  //   .skip(Number(desde))
+  //   .limit(Number(limite));
+
+  // const total = await Usuario.countDocuments(query);
+
+  const [total, usuarios] = await Promise.all([
+    Usuario.countDocuments(query),
+    Usuario.find(query).skip(Number(desde)).limit(Number(limite)),
+  ]);
+
   res.json({
+    total,
     usuarios,
   });
 };
